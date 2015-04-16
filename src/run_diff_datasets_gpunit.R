21640cd3ddff4c9a618615e054fb6b38b96bd669
David Eby <eby@broadinstitute.org>
2015-04-16 20:15:19 +0000

    Modified to check the row and column labels for diffs.  Added an R script that can be called from gpunit; this is still not fully portable but is closer than the previous one.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/DiffDatasets@35099 47fccdf2-e235-0410-87e5-63258e95d6e5

---
+++ src/run_diff_datasets_gpunit.R
@@ -0,0 +1,32 @@
+## The Broad Institute
+## SOFTWARE COPYRIGHT NOTICE AGREEMENT
+## This software and its documentation are copyright (2013) by the
+## Broad Institute/Massachusetts Institute of Technology. All rights are
+## reserved.
+##
+## This software is supplied without any warranty or guaranteed support
+## whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+## use, misuse, or functionality.
+
+args <- commandArgs(trailingOnly=TRUE)
+
+library(optparse)
+option_list <- list(
+  make_option("--first.input.file", dest="first.input.file"),
+  make_option("--second.input.file", dest="second.input.file"),
+  make_option("--round.method", dest="round.method"),
+  make_option("--round.digits", dest="round.digits", default=NULL)
+  )
+
+opt <- parse_args(OptionParser(option_list=option_list), positional_arguments=TRUE, args=args)
+print(opt)
+opts <- opt$options
+sessionInfo()
+
+all.args <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.dir <- dirname(sub(file.arg.name, "", all.args[grep(file.arg.name, all.args)]))
+source(file.path(script.dir, "common.R"))
+source(file.path(script.dir, "diff_datasets.R"))
+
+Diff.Datasets(opts$first.input.file, opts$second.input.file, opts$round.method, opts$round.digits)
