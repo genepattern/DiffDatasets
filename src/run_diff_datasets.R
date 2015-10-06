76a651668231eacefcc7b74069873415e410a3e6
David Eby <eby@broadinstitute.org>
2015-10-06 17:19:23 +0000

    Updated to make use of the R package installer.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/DiffDatasets@35309 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/run_diff_datasets.R
+++ src/run_diff_datasets.R
@@ -1,6 +1,6 @@
 ## The Broad Institute
 ## SOFTWARE COPYRIGHT NOTICE AGREEMENT
-## This software and its documentation are copyright (2013) by the
+## This software and its documentation are copyright (2015) by the
 ## Broad Institute/Massachusetts Institute of Technology. All rights are
 ## reserved.
 ##
@@ -8,15 +8,15 @@
 ## whatsoever. Neither the Broad Institute nor MIT can be responsible for its
 ## use, misuse, or functionality.
 
+suppressMessages(suppressWarnings(library(getopt)))
+suppressMessages(suppressWarnings(library(optparse)))
+
+sessionInfo()
+
 args <- commandArgs(trailingOnly=TRUE)
 
-vers <- "2.15"            # R version
 libdir <- args[1]
-server.dir <- args[2]
-patch.dir <- args[3]
 
-source(file.path(libdir, "loadRLibrary.R"))
-load.packages(libdir, patch.dir, server.dir, vers)
 source(file.path(libdir, "common.R"))
 
 option_list <- list(
@@ -29,8 +29,9 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list=option_list), positional_arguments=TRUE, args=args)
 print(opt)
 opts <- opt$options
-sessionInfo()
 
 source(file.path(libdir, "diff_datasets.R"))
 
 Diff.Datasets(opts$first.input.file, opts$second.input.file, opts$round.method, opts$round.digits)
+
+sessionInfo()
